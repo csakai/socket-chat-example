fb4e2c5bf5c55461806a04edde79bc92ae312862
Chris Sakai <cs.chris.sakai@gmail.com>
2015-07-01 17:31:11 -0400

    Add very simple chat saving support

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 var app = require('express')();
 var http = require ('http').Server(app);
 var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/chat', function (err){
+  if (err)
+    throw err;
+  else
+    console.log('connected to localhost:27017/chat');
+});
+
+var msgSchema = mongoose.Schema({
+  msg: String,
+  sent: { type: Date, default: Date.now }
+});
+
+var Msg = mongoose.model('Message', msgSchema);
 
 app.get('/', function (req, res){
   res.sendFile(__dirname + '/index.html');
 });
+var connectedUsers = {};
 
 io.on('connection', function (socket){
   socket.broadcast.emit('new user', 'a new user joined');
   socket.on('chat message', function (msg){
-    io.emit('chat message', msg);
+    var newMsg = new Msg({ msg : msg });
+    newMsg.save(function (err) {
+      if (err)
+        throw err;
+      else
+        io.emit('chat message', newMsg);
+    });
   });
   socket.on('disconnect', function (){
     socket.broadcast.emit('user disconnect', 'a user disconnected');
